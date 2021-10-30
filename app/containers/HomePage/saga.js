a3bff1b49668ed739146c8660c99401d12d2eba9
Serena Kong <mayplesap@Serenas-Air.localdomain>
2021-10-29 17:18:17 -0700

    pattern matched with default boilerplate sample to connect to server

---
+++ app/containers/HomePage/saga.js
@@ -0,0 +1,37 @@
+/**
+ * Gets array from Express backend
+ */
+
+import { call, put, takeLatest } from 'redux-saga/effects';
+import { LOAD_LIST } from 'containers/App/constants';
+import { listLoaded, listLoadingError } from 'containers/App/actions';
+
+import request from 'utils/request';
+
+/**
+ * Backend array request/response handler
+ */
+
+export function* getList() {
+  // const strings = yield select
+  const requestURL = `http://localhost:3001/strings`;
+
+  try {
+    // call request helper (see 'utils/request')
+    const list = yield call(request, requestURL);
+    console.log('in saga', list);
+    yield put(listLoaded(list));
+  } catch (err) {
+    yield put(listLoadingError(err));
+  }
+}
+
+/**
+ * Root saga manages watcher lifecycle
+ */
+
+export default function* listData() {
+  // using 'takeLatest' only the result of the latest API call is applied.
+  // watches for LOAD_LIST actions and calls getList when one comes in
+  yield takeLatest(LOAD_LIST, getList);
+}
