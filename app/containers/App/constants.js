300092f831718ca192841b67747666eb3a3d81a2
Serena Kong <mayplesap@Serenas-Air.localdomain>
2021-10-29 17:13:38 -0700

    added constants

--- app/containers/App/constants.js
+++ app/containers/App/constants.js
@@ -8,3 +8,7 @@
  * Follow this format:
  * export const YOUR_ACTION_CONSTANT = 'yourproject/YourContainer/YOUR_ACTION_CONSTANT';
  */
+
+export const LOAD_LIST = 'boilerplate/App/LOAD_LIST';
+export const LOAD_LIST_SUCCESS = 'boilerplate/App/LOAD_LIST_SUCCESS';
+export const LOAD_LIST_ERROR = 'boilerplate/App/LOAD_LIST_ERROR';
