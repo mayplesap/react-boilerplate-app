ddbe154014e90491a7a33fbe315d3dd6df89f461
Serena Kong <mayplesap@Serenas-Air.localdomain>
2021-10-29 17:19:44 -0700

    pattern matched with default boilerplate sample

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -5,14 +5,87 @@
  *
  */
 
-import React from 'react';
+import React, { useEffect, memo } from 'react';
+import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
 
-export default function HomePage() {
+import { useInjectReducer } from 'utils/injectReducer';
+import { useInjectSaga } from 'utils/injectSaga';
+import {
+  makeSelectList,
+  makeSelectLoading,
+  makeSelectError,
+} from 'containers/App/selectors';
+import CenteredSection from './CenteredSection';
+import Section from './Section';
+import messages from './messages';
+import { loadList } from '../App/actions';
+import reducer from '../App/reducer';
+import saga from './saga';
+
+const key = 'home';
+
+export function HomePage({ list, loading, error, onMount }) {
+  useInjectReducer({ key, reducer });
+  useInjectSaga({ key, saga });
+
+  const listProps = {
+    loading,
+    error,
+    list,
+  };
+
+  useEffect(() => {
+    if (list === false) {
+      onMount();
+    }
+  }, []);
+
+  console.log('in homepage');
+  console.log('LIST', list);
+  console.log('listprops', listProps);
+
   return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
+    <Section>
+      <CenteredSection>
+        <h1>
+          <FormattedMessage {...messages.header} />
+        </h1>
+      </CenteredSection>
+    </Section>
   );
 }
+
+HomePage.propTypes = {
+  loading: PropTypes.bool,
+  error: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  list: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  onMount: PropTypes.func,
+};
+
+const mapStateToProps = createStructuredSelector({
+  list: makeSelectList(),
+  loading: makeSelectLoading(),
+  error: makeSelectError(),
+});
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    onMount: () => {
+      dispatch(loadList());
+    },
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  memo,
+)(HomePage);
