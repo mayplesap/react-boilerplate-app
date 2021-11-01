310ba6eacf42768478da3ac53752fb0ae7e08d7b
Serena Kong <mayplesap@Serenas-Air.localdomain>
2021-11-01 16:10:13 -0700

    pattern matched to default boilerplate sample

---
+++ app/components/Header/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+// import A from './A';
+import NavBar from './NavBar';
+import HeaderLink from './HeaderLink';
+// import Banner from './banner.jpg';
+import messages from './messages';
+
+function Header() {
+  return (
+    <div>
+      {/* <A href="https://www.reactboilerplate.com/">
+        <Img src={Banner} alt="react-boilerplate - Logo" />
+      </A> */}
+      <NavBar>
+        <HeaderLink to="/">
+          <FormattedMessage {...messages.home} />
+        </HeaderLink>
+        <HeaderLink to="/add">
+          <FormattedMessage {...messages.add} />
+        </HeaderLink>
+      </NavBar>
+    </div>
+  );
+}
+
+export default Header;
