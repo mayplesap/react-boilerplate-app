a3628c8b6ca733bed2baab239c99cfb25223af4d
Serena Kong <mayplesap@Serenas-Air.localdomain>
2021-11-01 16:12:56 -0700

    added Header component to render

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -12,14 +12,17 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import Header from 'components/Header';
 
 import GlobalStyle from '../../global-styles';
 
 export default function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
+        {/* <Route path="/add" component={AddString} /> */}
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
