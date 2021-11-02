6c71461c7f8b36b90bcaf8a720025b9e85a34cbb
Serena Kong <mayplesap@Serenas-Air.localdomain>
2021-11-02 15:38:18 -0700

    added new message for adding new item

--- app/containers/AddString/messages.js
+++ app/containers/AddString/messages.js
@@ -12,4 +12,8 @@ export default defineMessages({
     id: `${scope}.header`,
     defaultMessage: 'This is the AddString container!',
   },
+  addNew: {
+    id: `${scope}.add.new`,
+    defaultMessage: 'Add new item',
+  },
 });
