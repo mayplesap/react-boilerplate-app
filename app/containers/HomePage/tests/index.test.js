f5b54cb7a71cf52cf18599675ae1a652c2bcb91a
Serena Kong <mayplesap@Serenas-Air.localdomain>
2021-11-01 17:48:21 -0700

    updated test for snapshot

--- app/containers/HomePage/tests/index.test.js
+++ app/containers/HomePage/tests/index.test.js
@@ -1,17 +1,32 @@
+/**
+ * Test the HomePage
+ */
+
 import React from 'react';
 import { render } from 'react-testing-library';
 import { IntlProvider } from 'react-intl';
+import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router-dom';
 
 import HomePage from '../index';
+import configureStore from '../../../configureStore';
 
 describe('<HomePage />', () => {
+  let store;
+
+  beforeAll(() => {
+    store = configureStore({}, browserHistory);
+  });
+
   it('should render and match the snapshot', () => {
     const {
       container: { firstChild },
     } = render(
-      <IntlProvider locale="en">
-        <HomePage />
-      </IntlProvider>,
+      <Provider store={store}>
+        <IntlProvider locale="en">
+          <HomePage />
+        </IntlProvider>
+      </Provider>,
     );
     expect(firstChild).toMatchSnapshot();
   });
