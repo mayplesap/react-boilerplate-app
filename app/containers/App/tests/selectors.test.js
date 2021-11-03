fd1060944c7cda9e5cbf7cc236697aab1df18815
Serena Kong <mayplesap@Serenas-Air.localdomain>
2021-11-02 18:46:08 -0700

    added tests for makeSelectLoading and makeSelectError

--- app/containers/App/tests/selectors.test.js
+++ app/containers/App/tests/selectors.test.js
@@ -31,15 +31,41 @@ describe('selectGlobal', () => {
 describe('makeSelectList', () => {
   const listSelector = makeSelectList();
   it('should select the list', () => {
-    const testString = 'test';
+    const strings = 'test';
     const mockedState = {
       global: {
-        strings: testString,
+        strings,
       },
     };
 
-    expect(listSelector(mockedState)).toEqual(testString);
+    expect(listSelector(mockedState)).toEqual(strings);
   });
+});
 
-  console.log(makeSelectLoading, makeSelectError);
+describe('makeSelectLoading', () => {
+  const loadingSelector = makeSelectLoading();
+  it('should select the loading', () => {
+    const loading = false;
+    const mockedState = {
+      global: {
+        loading,
+      },
+    };
+
+    expect(loadingSelector(mockedState)).toEqual(loading);
+  });
+});
+
+describe('makeSelectError', () => {
+  const errorSelector = makeSelectError();
+  it('should select the error', () => {
+    const error = 404;
+    const mockedState = {
+      global: {
+        error,
+      },
+    };
+
+    expect(errorSelector(mockedState)).toEqual(error);
+  });
 });
